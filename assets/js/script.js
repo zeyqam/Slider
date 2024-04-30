540009f6af3830219785c77b2bfc0a009760d9a8
zeyqam <zegamda@code.edu.az>
2024-05-01 01:46:06 +0400

    end task

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,6 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  let i;
   let slides = document.getElementsByClassName("mySlides");
 
   if (n > slides.length) {
@@ -20,7 +19,7 @@ function showSlides(n) {
   if (n < 1) {
     slideIndex = slides.length;
   }
-  for (i = 0; i < slides.length; i++) {
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
 
